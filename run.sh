723b0b4131ac2855c1732635f9bba2814b552694
annsftr <152196504+annsftr@users.noreply.github.com>
2024-03-27 11:04:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-24411ea1-1bb4-4a5c-8284-b1da76ce8fed/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-24411ea1-1bb4-4a5c-8284-b1da76ce8fed//)
 
 
 echo "$curl_output" >> Update.tsx
